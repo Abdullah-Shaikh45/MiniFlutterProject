c99bca0f26c5c12e5597821a4b35912c68b89abe
Abdullah Shaikh <cloudyab854@gmail.com>
2024-11-26 21:09:10 +0530

    made perview for the app

--- my_weather_app/lib/main.dart
+++ my_weather_app/lib/main.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:my_weather_app/pages/weather_page.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      backgroundColor: Colors.white,
+      enabled: true,
+      defaultDevice: Devices.ios.iPhone13ProMax,
+      isToolbarVisible: true,
+      availableLocales: [Locale('en', 'US')],
+      tools: const [
+        DeviceSection(
+          model: true,
+          orientation: false,
+          frameVisibility: false,
+          virtualKeyboard: false,
+        ),
+      ],
+      devices: [
+        Devices.android.samsungGalaxyA50,
+        Devices.android.samsungGalaxyNote20,
+        Devices.android.samsungGalaxyS20,
+        Devices.android.samsungGalaxyNote20Ultra,
+        Devices.android.onePlus8Pro,
+        Devices.android.sonyXperia1II,
+        Devices.ios.iPhoneSE,
+        Devices.ios.iPhone12,
+        Devices.ios.iPhone12Mini,
+        Devices.ios.iPhone12ProMax,
+        Devices.ios.iPhone13,
+        Devices.ios.iPhone13ProMax,
+        Devices.ios.iPhone13Mini,
+      ],
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +43,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Weather App',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home: WeatherPage(),
     );
